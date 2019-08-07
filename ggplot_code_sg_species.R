27615d973cbe51f245906696c21f0e3158bd11f9
Irina Birskis Barros <irina.bbarros@gmail.com>
2019-08-07 16:10:25 -0700

    changing the label again

--- ggplot_code_sg_species.R
+++ ggplot_code_sg_species.R
@@ -17,24 +17,19 @@ m_fen$gen = rep(1:100, 5*10)
 m_fen$species <- rep(seq(from=1, to=50), each=100)
 m_fen$teta <- rep(teta.sp, each=100)
 m_fen = separate(m_fen, variable, c("habitat", "species_habitat"))
-#for a number of specialists = 5 
-for(a in 1:5){
-  m_fen$habitat[which(m_fen$species==sg[a])] <- "SG"
-}
+
+m_fen$habitat[which(m_fen$species==sg[a])] <- "SG"
 
 teta.df <- data.frame(teta=unique(teta.sp))
-
-#for a number of specialists = 5 
-for (i in 1:5){
-  if (teta.sp[sg[i]] == teta.df$teta) {
-    teta.df$habitat[teta.sp[sg[i]] == teta.df$teta] = "SG"
-  }
-}
+teta.df$habitat[unique(teta.sp[sg]) == teta.df$teta] = "SG"
 teta.df$habitat[is.na(teta.df$habitat)] <- c(LETTERS[1:10])
 
 ggplot(m_fen, aes(gen, value, 
                   group = interaction(habitat, species), 
                   color = habitat)) + 
   geom_line() + theme_classic() + 
-  geom_hline(data = teta.df, aes(yintercept = teta, color = habitat), linetype = 2)+
+  geom_hline(data = teta.df, aes(yintercept = teta, color = habitat), linetype = "dashed")+
   theme(axis.text = element_text(size = 12)) 
+
+ggsave("/Users/irinabb/Dropbox/Mestrado/Am_Nat/Revisao1/new_figures/FigS8c.pdf", plot = last_plot(), width=5.92, height=4.49)
+
